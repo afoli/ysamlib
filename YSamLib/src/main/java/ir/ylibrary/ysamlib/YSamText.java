04f07913f1d7706c8b5f16e7c1737b243567c05a
afsaneh <afsaneh.fallahzadeh69@gmail.com>
2023-09-07 12:35:25 +0330

    commit from v 0.0.2

--- YSamLib/src/main/java/ir/ylibrary/ysamlib/YSamText.java
+++ YSamLib/src/main/java/ir/ylibrary/ysamlib/YSamText.java
@@ -9,20 +9,33 @@ import androidx.annotation.Nullable;
 public class YSamText extends androidx.appcompat.widget.AppCompatTextView {
     public YSamText(@NonNull Context context) {
         super(context);
+        setTextColor(0);
     }
 
     public YSamText(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        setTextColor(0);
     }
 
     public YSamText(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        setTextColor(0);
     }
 
     @Override
-    public void setTextSize(int unit, float size) {
-        size = 200;
-        super.setTextSize(unit, size);
+    public void setTextColor(int color) {
+        color = getResources().getColor(R.color.textColorExample, null);
+        super.setTextColor(color);
+    }
 
+    @Override
+    public void setText(CharSequence text, BufferType type) {
+        text = "library";
+        super.setText(text, type);
+    }
+
+    @Override
+    public boolean isInEditMode() {
+        return super.isInEditMode();
     }
 }
